dc2e5423ecc3083f4bf40be3815f3cfc28df5e4b
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-01 18:46:49 +0530

    Create hcf.java

---
+++ hcf.java
@@ -0,0 +1,19 @@
+class hcf
+{
+public static void main(String args[])
+{
+ System.out.println("Enter 2 numbers");
+ Scanner scan=new Scanner(System.in);
+ int m=scan.nextInt();
+ int n=scan.nextInt();
+ for(int i=2;i<p;i++)
+ {
+ if((m%i==0)&&(n%i==0))
+ {
+ h=i;
+ }
+ }
+ System.out.println("HCF="+h);
+ }
+ }
+ 
