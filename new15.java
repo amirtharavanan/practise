dac7e3bf6bc0da1449fb11f9ff69415f45341e20
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-01 16:49:47 +0530

    Create new15.java

---
+++ new15.java
@@ -0,0 +1,16 @@
+public class new15
+{
+public static void main(String args[])
+{
+string orig,rev="";
+Scanner scan=new Scanner(System.in);
+System.out.print("Enter a sring to reverse :");
+orig=scan.nextline();
+len=orig.len();
+for(i=len-1;i>0;i--)
+{
+rev=rev+orig.charAt(i);
+}
+System.out.print("Reverse of Entered String is: "+rev);
+}
+}
