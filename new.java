b7e822845600b9da959fd2df66d8214297d94b7f
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-01 10:40:23 +0530

    Create new.java

---
+++ new.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+class oddeven
+{
+public static void main(String args[])
+{
+int x;
+System.out.println("Enter the number");
+Scanner in=new Scanner(System.in);
+x=in.nextInt()
+if(x%2==0)
+System.out.println("EVEN NUMBER");
+else
+System.out.println("ODD NUMBER");
+}
+}
