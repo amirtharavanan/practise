b8326ccbcadcfb54e76f5921e045e3aea6a7f94b
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-02 21:02:39 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+int num,sum=0,i;
+cout<<"Enter any number:";
+cin>>num;
+{
+for(i=0;i<=num;i++)
+sum+=i;
+}
+cout<<"Sum of all numbers up to ""<<num<<"is"<<sum<<endl;
+system("pause");
+return 0;
+}
