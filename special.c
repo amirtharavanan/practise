c8a28e14a1f0ddaa8fb83e77aa894e161418ea14
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-03 18:09:13 +0530

    Create special.c

---
+++ special.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#define  MAX_SIZE 100
+int main()
+{
+char string[MAX_SIZE];
+int alphabets,digits,others,i;
+alphabets=digits=others=i=0;
+printf("Enter any string:");
+gets(string);
+while(strng[i]!='\0')
+{
+if((string[i]>='a' && string[i]<='z')||(string[i]>='A' && string[i]<='Z'))
+{
+alphabets++;
+}
+else if(string[i]>='0' && string[i]<='9')
+{
+digit++;
+}
+else
+{
+others++;
+}
+i++;
+}
+printf("Special charecters=%d\n",others);
+return 0;
+}
