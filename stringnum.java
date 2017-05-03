82b3a356f7a49b82c923815ec4adcab47b455a6d
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-03 21:15:48 +0530

    Update stringnum.java

--- stringnum.java
+++ stringnum.java
@@ -7,7 +7,7 @@ char ch[]=new char[s.length()];
 for(i=o;i<s.length();i++)
 {
 ch[i]=s.charAt(i);
-if(((i>0)&&(ch[i]!='')&&(ch[i-1]==''))
+if(((i>0)&&(ch[i]!='')&&(ch[i-1]==''))||((ch[0]!='')&&(i==0)))
 c++;
 }
 return c;
