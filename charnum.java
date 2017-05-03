f7ff2679090031d29eacf44242dd2fea6748d9c5
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-03 20:48:53 +0530

    Update charnum.java

--- charnum.java
+++ charnum.java
@@ -5,4 +5,13 @@ public class charnum
 public static void main(String args[])
 {
 Scanner in=new Scanner(System.in);
-Systemout.prin
+Systemout.println("Enter string for counting:");
+  String input=in.next();
+  int chars=0;
+  while(in.hasNextByte())
+  {
+    in.nextByte();
+    chars++;
+  }
+}
+}
