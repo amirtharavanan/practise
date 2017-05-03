c93aa452c9b8e1f5d37254dd5f54c32cf6fda6d8
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-03 15:03:45 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int sum;
+for(int i=1;i<15;i++)
+{
+sum+=i;
+}
+printf("%d",sum);
+for(int i=15;i<45;i++)
+{
+if(i%2!=0)
+{
+int odd=0;
+odd=odd+i;
+printf("Sum of odd numbers=%d",odd);
+getch();
+}
+}
+}
