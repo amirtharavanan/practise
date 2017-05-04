1ce722d4193c75491400eb7f2793ab0c84f2062f
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-04 10:54:08 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class table
+{
+public static void main(String args[])
+{
+Scanner scan=new Sanner(System.in);
+System.out.println("Enter Number");
+int n=scan.nextInt();
+for(int i=0;i<=10;i++)
+{
+System.out.println(n+"*"+i+"=+n*i);
+}
+}
+}
