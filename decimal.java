01308aafb7d0b20bd659aa271a065cd5c0eb40f4
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-04 13:01:20 +0530

    Create decimal.java

---
+++ decimal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class decimal
+{
+public static void main(String args[])
+{
+int decnum,rem,quot,i=1,j;
+int binnum[]=new int[100];
+Scanner scan=new Scanner(System.in);
+System.out.print("Enter any Decimal Number;");
+decnum=scan.nextInt();
+quot=decnum;
+while(quot!=0)
+{
+binnum[i++]=quot%2;
+quot=quot/2;
+}
+System.out.print("Equivalent Binary value of"+decnum+"is:\n");
+for(j=i-1;j>0;j--)
+{
+System.out.print(binnum[j]);
+}
+}
+}
