5c31714909969d94175fa4946fa90806681938c7
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-05 12:18:25 +0530

    Create cavg.c

---
+++ cavg.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int Arr[100],n,i,sum=0;
+printf("Enter the number of elements");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("Enter element %d:");
+scanf("%d",&Arr[i]);
+sum+=Arr[i];
+}
+printf("\nsum of the array is:%d,sum");
+printf("\nAverage of the array is:%0.2f",(float/n);
+return 0;
+}
+
