fefbd6787cfabca598ab72c9c4f35a14f13597b2
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-05 15:56:07 +0530

    Create ap.java

---
+++ ap.java
@@ -0,0 +1,20 @@
+class ap
+{
+public static void main(String args[])
+{
+int a,d,n=1,t,b;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the first term of AP");
+ac=sc.nextInt();
+System.out.println("Enter the difference");
+d=sc.nextInt();
+System.out.println("Enter the number of terms");
+b=sc.nextInt();
+while(b!=n-1)
+{
+t=(a+(n-1)*d);
+n++;
+System.out.print(t+" ");
+}
+}
+}
