0bdf8740735d6cb074b95c1ce9f555d9284fbe1a
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-05 16:20:50 +0530

    Create npr.java

---
+++ npr.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class npr
+{
+public static int fact(int num)
+{
+int fact=1,i;
+for(i=1;i<=um;i++)
+{
+fact=fact*i;
+}
+return fact;
+}
+public static void main(String args[])
+{
+int n,r;
+Scanner scan=new Scanner(System.in);
+System.out.print("Enter value of n: ");
+n=scan.nextInt();
+System.out,print("Enter the value of r:");
+r=scan.nextInt();
+System.out.print("\n NPR="+(fact(n)/(fact(n-r)));
+}
+}
