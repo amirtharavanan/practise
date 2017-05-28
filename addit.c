57b92fcfac0de58a1584e828fde5e1e6a382bec7
amirtharavanan <amirtha.ravanan@gmail.com>
2017-05-28 12:54:25 +0530

    Create addit.c

---
+++ addit.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a[5],b[5],sum[5];
+clrscr();
+printf("\n Reading the 1st array\n");
+for(i=0;i<5;i++)
+{
+printf("Enter the value");
+scanf("%d",&a[i]);
+}
+printf("\n reading the secnd array\n");4
+for(i=0;i<5;i++)
+{
+printf("Enter the value");
+scanf("%d",&b[i]);
+}
+printf("\n the sum is\n");
+for(i=0;i<5;i++)
+{
+sum=a[i]+b[i];
+printf("\n the sum is %d & %d is %d",a[i],b[i],c[i]);
+}
+getch();
+}
+
