ce4c8312a9d2d0cde7a5d11fe09a96f6ab7e5f9c
amirtharavanan <amirtha.ravanan@gmail.com>
2017-06-03 15:41:55 +0530

    Create Working.java

---
+++ Working.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Working
+{
+public static void main(String args[])
+{
+String s=args[0];
+if(s="Monday"||s="Tuesday"||s="Wednesday"||s="Thursday"||s="Friday"||s="Saturday")
+{
+System.out.println("Workingday("+s+")->True");
+}
+else
+{
+Systen.out.println()
