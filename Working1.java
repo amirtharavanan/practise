9e00e930382faf34f8501e279f589696f0a5f347
amirtharavanan <amirtha.ravanan@gmail.com>
2017-06-04 14:57:30 +0530

    Create Working1.java

---
+++ Working1.java
@@ -0,0 +1,32 @@
+import java.io.*;
+imor java.util.*;
+public class Working
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+switch(s)
+{
+case"Sunday":
+System.out.println("Workingday(sunday)->false");
+break;
+case"Sunday":
+System.out.println("Workingday(sunday)->false");
+break;
+case"Sunday":
+System.out.println("Workingday(sunday)->false");
+break;
+case"Sunday":
+System.out.println("Workingday(sunday)->false");
+break;
+case"Sunday":
+System.out.println("Workingday(sunday)->false");
+break;
+case"Sunday":
+System.out.println("Workingday(sunday)->false");
+break;
+case"Sunday":
+System.out.println("Workingday(sunday)->false");
+break;
+
