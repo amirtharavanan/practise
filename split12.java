5f32bc2261939f28428cbe86a7bd17835dcbe5a9
amirtharavanan <amirtha.ravanan@gmail.com>
2017-06-06 07:18:12 +0530

    Update split12.java

--- split12.java
+++ split12.java
@@ -2,5 +2,10 @@ class split12
 {
 public static void main(String args[])
 {
-String s=args[0].split(" ");
-fpr
+String[] s=args[0].split(" ");
+for(int i=args[0].split(" ").length-1;i>=0;i--)
+{
+  System.ou.println(s[i]);
+}
+}
+}
