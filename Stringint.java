c4f14e288524741b371c2ac78d0c5a9079c213eb
amirtharavanan <amirtha.ravanan@gmail.com>
2017-06-06 11:55:01 +0530

    Update Stringint.java

--- Stringint.java
+++ Stringint.java
@@ -1,2 +1,12 @@
 import java.io.*;
-import java.uil.*;
+import java.util.*;
+public class Stringint
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+    int i=Integer.parseInt(s);
+    System.out.println(i);
+  }
+}
