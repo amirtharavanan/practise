75f5940afe31508b3aa17ef78872b101ef21ceca
amirtharavanan <amirtha.ravanan@gmail.com>
2017-06-06 07:18:58 +0530

    Update split12.java

--- split12.java
+++ split12.java
@@ -5,7 +5,7 @@ public static void main(String args[])
 String[] s=args[0].split(" ");
 for(int i=args[0].split(" ").length-1;i>=0;i--)
 {
-  System.ou.println(s[i]);
+  System.out.println(s[i]);
 }
 }
 }
