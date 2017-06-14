5190da263ecdab88288ef08e0cd382b30def65e8
amirtharavanan <amirtha.ravanan@gmail.com>
2017-06-14 17:10:01 +0530

    Create Prime1.java

--- Prime1.java
+++ Prime1.java
@@ -30,4 +30,17 @@ break;
 number++;
 }
 System.out.println(" ");
+}
+  private static boolean isPrimeNumber(int number){
+	         
+	        for(int i=2; i<=number/2; i++){
+	            if(number % i == 0){
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+
+	}
+
 
